6d03aeab92cbd923aeeea23f574a515057f3f2ec
Gaurav <gaurav11018@gmail.com>
2022-06-20 14:12:13 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import { ConnectButton } from "web3uikit";
+import logo from "./images/download.png";
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <div className="Header">
+        <div className="logo">
+          <img src={logo} alt="logo" height="50px" />
+          Sentiment
+          <ConnectButton />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;
